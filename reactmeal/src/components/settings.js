6bbaf4d7cac66fc949d09d5edcc70468e648c76f
ljensen63 <liam.jensen63@gmail.com>
2022-09-29 00:42:58 -0400

    add folder for component pages

---
+++ reactmeal/src/components/settings.js
@@ -0,0 +1,13 @@
+import { TopNav } from '../topNav'
+import { SideNav } from '../sideNav'
+import { SearchNav } from '../searchNav'
+
+export const Settings = () => {
+    return (
+        <>
+            <TopNav/>
+            <SideNav/>
+            <div>This is the settings homepage</div>
+        </>
+    );
+}
