8f6dec8dd9c733c68a8eabf97829b523073317ba
ljensen63 <liam.jensen63@gmail.com>
2022-10-08 14:30:05 -0400

    add search UI and some styling

--- reactmeal/src/searchNav.js
+++ reactmeal/src/searchNav.js
@@ -4,43 +4,57 @@ export const SearchNav = () => {
         <>
             <div className="searchnav">
                 <form>
-                    <div class="pantry-switch">
-                        <input type="checkbox" class="toggle-switch-checkbox" name="toggleSwitch" id="toggleSwitch" />
-                        <label class="toggle-switch-label" for="toggleSwitch">
+                    <form class="pantry-switch">
+                        <input type="checkbox" className="toggle-switch-checkbox" name="toggleSwitch" id="toggleSwitch" />
+                        <label className="toggle-switch-label" for="toggleSwitch">
                             Use Pantry
                         </label>
-                    </div>
-                    <div class="vegan-switch">
+                    </form>
+                    <form class="vegan-switch">
                         <input type="checkbox" class="toggle-switch-checkbox" name="toggleSwitch" id="toggleSwitch" />
                         <label class="toggle-switch-label" for="toggleSwitch">
                             Vegan
                         </label>
-                    </div>
-                    <div class="gf-switch">
+                    </form>
+                    <form class="gf-switch">
                         <input type="checkbox" class="toggle-switch-checkbox" name="toggleSwitch" id="toggleSwitch" />
                         <label class="toggle-switch-label" for="toggleSwitch">
                             Gluten Free
                         </label>
-                    </div>
-                    <div class="custom-switch">
+                    </form>
+                    <form class="custom-switch">
                         <input type="checkbox" class="toggle-switch-checkbox" name="toggleSwitch" id="toggleSwitch" />
                         <label class="toggle-switch-label" for="toggleSwitch">
                             Custom Diet
                         </label>
-                    </div>
-                    <div class="slidecontainer">
-                        <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
+                    </form>
+                    <form class="slidecontainer">
+                        <input type="range" min="1" max="100"  id="myRange"/>
+                        <br/>
                         <label class="toggle-switch-label" for="toggleSwitch">
                             Rating
                         </label>
-                    </div>
-                    <div class="slidecontainer">
-                        <input type="range" min="1" max="100" value="50" class="slider" id="myRange"/>
-                        <label class="toggle-switch-label" for="toggleSwitch">
-                            Difficulty
-                        </label>
-                    </div>
+                    </form>
+                    <form>
+                        <input type="range" id="vol" name="vol" min="0" max="50"/>
+                        <br/>
+                        <label for="vol">Volume (between 0 and 50):</label>
+                        
+                    </form>
+                    
+                    <form>
+                        <label for="quantity">Number of Ingredients (between 1 and 5):</label>
+                        <input type="number" id="quantity" name="quantity" min="1" max="5"/>
+                    </form>
+
+                    <form>
+                        <label for="quantity">Recipe Time (between 1 and 5):</label>
+                        <input type="number" id="quantity" name="quantity" min="1" max="5"/>
+                    </form>
+
                 </form>
+                   
+                   
             </div>
 
         </>
