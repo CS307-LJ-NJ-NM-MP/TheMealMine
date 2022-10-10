35dd5dda582613066f357bbdf0cbe7b77511e1b1
Nathan Ma <emailnmma@gmail.com>
2022-10-10 04:51:31 -0400

    Updated add friends page

--- reactmeal/src/components/addFriend.js
+++ reactmeal/src/components/addFriend.js
@@ -1,15 +1,21 @@
 import { TopNav } from '../topNav'
 import { SideNav } from '../sideNav'
-
+import { SearchNav } from '../searchNav';
+import { FriendNav } from '../friendNav';
+import { ReactDOM } from 'react-dom/client';
 
 export const Friends = () => {
+
     return (
+        
         <>
+            <div>
             <TopNav/>
-            <div>Friends List</div>
-            <button> Add Friend </button>
+
+            Friends List:
             <SideNav/>
-            
+            <FriendNav/>
+            </div>
         </>
     );
 }
