7b72eec7819ff4ef499022abde86929db68a0ace
Nathan Ma <emailnmma@gmail.com>
2022-10-13 00:00:40 -0400

    alpha version of background logo

--- reactmeal/src/Home.js
+++ reactmeal/src/Home.js
@@ -2,16 +2,18 @@ import { TopNav } from './topNav'
 import { SideNav } from './sideNav'
 import { SearchNav } from './searchNav'
 import { Pantry } from './addIngredients'
+import background from "./imgs/homeLogoCopy.png"
 
 export const Home = () => {
     return (
         <>
-            
-            <TopNav/>
-            <div className="home-display">
-                <SearchNav/>
-                <Pantry/>
-                <SideNav/>
+            <div style={{ backgroundImage: `url(${background})` }}>
+                <TopNav/>
+                <div className="home-display">
+                    <SearchNav/>
+                    <Pantry/>
+                    <SideNav/>
+                </div>
             </div>
             
         </>
