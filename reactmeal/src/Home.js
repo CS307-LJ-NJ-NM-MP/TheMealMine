ff2e44cfd851c328e32a73e83528fecbed65b0a2
GeneralCypher <john2791@purdue.edu>
2022-10-13 20:23:03 -0400

    pantry update

--- reactmeal/src/Home.js
+++ reactmeal/src/Home.js
@@ -2,12 +2,11 @@ import { TopNav } from './topNav'
 import { SideNav } from './sideNav'
 import { SearchNav } from './searchNav'
 import { Pantry } from './addIngredients'
-import background from "./imgs/homeLogoCopy.png"
 
 export const Home = () => {
     return (
         <>
-            <div style={{ backgroundImage: `url(${background})` }}>
+            <div>
                 <TopNav/>
                 <div className="home-display">
                     <SearchNav/>
@@ -15,7 +14,6 @@ export const Home = () => {
                     <SideNav/>
                 </div>
             </div>
-            
         </>
     );
 }
