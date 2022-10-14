d241b0e7d2b887324d0044dbfed6874e014232ce
GeneralCypher <john2791@purdue.edu>
2022-10-13 22:04:33 -0400

    chakra push

--- reactmeal/src/addIngredients.js
+++ reactmeal/src/addIngredients.js
@@ -1,13 +1,43 @@
+import { Box, Container, Text } from "@chakra-ui/react";
 import React from "react";
+
 
 export const Pantry = () => {	
 return (<>
-	<div>
-		<input placeholder="Add Ingredients Here" type="text"/><button>Search</button><br/>
-		<div>
-			<br/>
-			<input placeholder="Add Pantry Items Here" type="text"/><button>Add</button>
-		</div>
-	</div>
+	<Container maxW='xl' centerContent>
+		<Box d='flex'
+			justifyContent='center'
+			p={3}
+			bg={"light grey"}
+			w="100%"
+			m="40px 0 15px 0"
+			borderRadius="lg"
+			borderWidth="1px"
+		>
+			<Text fontSize="1xl" color="black">Search</Text>
+		</Box>
+		<Box d='flex'
+			justifyContent='center'
+			p={3}
+			bg={"light grey"}
+			w="100%"
+			m="40px 0 15px 0"
+			borderRadius="lg"
+			borderWidth="1px"
+		>
+			<Text fontSize="2xl" color="black">Pantry</Text>
+		</Box>
+		<Box d='flex'
+			justifyContent='center'
+			p={3}
+			bg={"light grey"}
+			w="100%"
+			m="40px 0 15px 0"
+			borderRadius="lg"
+			borderWidth="1px"
+		>
+			<Text fontSize="4xl" color="black">Database</Text>
+		</Box>
+	</Container>
 </>
 );}
