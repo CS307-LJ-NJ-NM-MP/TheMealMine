fef29140b6a3eb7aa09ae2c381ec00e574030405
MadelinePOBrien <mpietran@purdue.edu>
2022-11-01 15:41:26 -0400

    First attempt, hooking up button to unfollow

--- reactmeal/src/pages/FriendsPage.js
+++ reactmeal/src/pages/FriendsPage.js
@@ -3,13 +3,17 @@ import { Box, Container, Text, Stack, Input, Button, HStack, VStack } from "@cha
 import { TopNav } from "../topNav";
 import { SideNav } from "../sideNav";
 import { useState } from "react";
+import e from "cors";
+import Axios from "axios";
 
 function FriendsPage() {
     var username = localStorage.getItem('username');
     var password = localStorage.getItem('password');
     var friendsList;
     var blockedList;
+    //var tempName = localStorage.setItem('tempName', "guest");
     console.log("New Refresh");
+    
     //const [friendsListForm, setFriendsListForm] = useState({
       //  friendsList: '',
     //});
@@ -37,13 +41,34 @@ function FriendsPage() {
 	}
     const handleUnfollow = (name) => {
         //Send request to server to update mongo and remove that person from their friendslist
+
+    }
+    
+    async function unfollow(e) {
+        e.preventDefault();
+        const nameToUnfollow = e.target.value;
+        console.log("Unfollowing: " + nameToUnfollow);
+        var result = await Axios.post('http://localhost:5000/unfollow', {
+				user: username,
+				name: nameToUnfollow
+			});
+        window.location.reload(true);
+    }
+    const handleUnblock = (name) => {
+
+    }
+    const handleBlock = (name) => {
+    
     }
 
     const FriendDisplay = (name) => {
         return (
         <HStack key={name} width="400px" spacing="10px" border-style="solid">
             <Text width="200px">{name}</Text>
-            <Button align="right" color="blue">Unfollow</Button>
+            <Button value={name} align="right" color="blue" 
+                onClick={unfollow(e, name)}>
+                    Unfollow</Button>
+            <Button align="right" color="blue">Block</Button>
         </HStack>
         );
     }
