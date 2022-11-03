b4061ff94050bdcf95705de3b97d490416f77284
Nathan Ma <emailnmma@gmail.com>
2022-11-03 04:37:41 -0400

    pot bug fix with cat

--- reactmeal/src/categoryNav.js
+++ reactmeal/src/categoryNav.js
@@ -39,7 +39,7 @@ export function CategoryNav () {
 		if(userForm !== '') {
             console.log("valid: " + userForm);
 			var result = await Axios.post('http://localhost:5000/addCategory', {
-				user: userForm,  
+				user: localStorage.getItem('username'),  
                 recipe: "Cheese",
                 category: categoryForm.category
 			})
@@ -74,103 +74,3 @@ export function CategoryNav () {
 
 
 }
-
-//     const [name, setName] = useState("");
-
-
-//     const [list, setList] = useState(FriendsList);
-
-//     function sendRequest() {
-
-//         handleAdd();
-//         alert("Friend Request Sent to: " + name);
-//         setName("");
-//     }
-
-
-//     function handleAdd() {
-//         const newList = list.concat({name});
-//         setList(newList);
-//     }
-
-
-//     const handleSubmit = event => {
-//         event.preventDefault();
-// //        setName('');
-//     }
-
-                /* {list.map(data => (
-                    <p>{data.name}</p>
-                ))}
-
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        
-                        <label >Send Friend Request:</label>
-
-                        <input
-                        type="text" 
-                        onChange={event => setName(event.target.value)}
-                        value={name}
-                        />
-
-                    </div>
-                    <div>
-                        <input onClick={sendRequest} type="button" value="Send Friend Request" id="friendButton">
-
-                        </input>
-
-                    </div>
-
-                </form> */
-
-//     const dataList = Data.filter(username => {
-//         if (query === "") {
-// //            alert("User not found");
-//         }
-//         else if (username.user.toLowerCase().includes(query.toLowerCase())) {
-//             return username;
-//         }
-//     }).map((username) => (
-//         <div key={username.id}>
-//         <p>{username.user}</p>
-//         </div>
-//     ));
-    
-
-
-// async function findUser(e) {
-//     e.preventDefault();
-//     console.log("sending");
-//     if(formValue.user !== '') {
-//         console.log("valid: " + formValue.user);
-//         var result = await Axios.post('http://localhost:5000/findUserReg', {
-//             user: formValue.user,  
-//         })
-//         .then(response => {
-//             console.log("result: " + response.data);
-//             console.log("other result: " + result)
-// //                console.log("response: " + response);
-//             if (response.data !== "") {
-//                 console.log("form value: " + response.data.user)
-//                 setQuery(response.data.user);
-//                 console.log("final query: " + query)
-// //                    alert("Users " + response.data.user + " found");
-//                 setTextOut("" + response.data.user)
-//                 setQuery("");
-//             }
-//             else {
-//                 alert("error, user not valid")
-//                 setQuery("");
-//             }
-
-//         })
-//         .catch(error => {
-//             console.log(error.data)
-//             alert("error");
-//         });
-//     }
-//     else {
-//         alert ("No query");
-//     }
-// }
