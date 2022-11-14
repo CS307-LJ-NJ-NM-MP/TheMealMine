b9870a9a520b3cfa5e0d92aa2a015bbe42640043
ljensen63 <liam.jensen63@gmail.com>
2022-11-13 20:17:09 -0500

    add page to search for recipes

--- reactmeal/src/App.js
+++ reactmeal/src/App.js
@@ -10,6 +10,7 @@ import { PWReset } from "./components/pwReset"
 import SigninPage from './pages/SigninPage';
 import FriendsPage from './pages/FriendsPage';
 import { Notifications } from './components/notifications';
+import  SearchRecipes  from './components/searchRecipes'
 
 window.$username = '';
 
@@ -26,6 +27,7 @@ function App() {
 		    <Route path='/recovery' element={<Recovery/>} ></Route>
         <Route path='/recipeAdd' element={<RecipeAdd/>} ></Route>
         <Route path='/notifications' element={<Notifications/>} ></Route>
+        <Route path='/searchRecipes' element={<SearchRecipes/>} ></Route>
       </Routes>
     </>
   );
