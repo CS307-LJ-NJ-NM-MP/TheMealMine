f8072486593c574ab0ce511354d6ba5c4e0d80ba
Nathan Ma <emailnmma@gmail.com>
2022-11-30 23:04:19 -0500

    friends yay

--- reactmeal/src/pages/FriendsPage.js
+++ reactmeal/src/pages/FriendsPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container, Text, Stack, Input, 
+import { Box, Container, Text, Image, Input, 
         Center, Button, HStack, VStack, FormLabel, Divider } from "@chakra-ui/react";
 import { TopNav } from "../topNav";
 import { SideNav } from "../sideNav";
@@ -23,32 +23,14 @@ function FriendsPage() {
     var iS = localStorage.getItem('isSearching');
 
     if (iS === "no") {
-			var userFriendsList = localStorage.getItem('friendsList');
             var usersBlockedList = localStorage.getItem('blockedList');
-			if(userFriendsList !== null) {
-			    friendsList = userFriendsList.split(",");
-			} else {
-				friendsList = [];
-			}
             if (usersBlockedList !== null) {
                 blockedList = usersBlockedList.split(",");
             } 
     } else {
-        blockedList = localStorage.getItem('searchingBlocked').split(",")
-        friendsList = localStorage.getItem('searchingFriends').split(",");
+        blockedList = localStorage.getItem('searchingBlocked').split(",");
     }
 
-    async function unfollow(e) {
-        e.preventDefault();
-        const nameToUnfollow = e.target.value;
-        var result = await Axios.post('http://localhost:5000/unfollow', {
-				user: username,
-				name: nameToUnfollow,
-                id: Id,
-		});
-        localStorage.setItem('friendsList', result.data.friendsList);
-        setDoRender(e.target.value);
-    }
     async function follow(e) {
         e.preventDefault();
         var result = await Axios.post('http://localhost:5000/follow', {
@@ -65,6 +47,7 @@ function FriendsPage() {
         localStorage.setItem('isSearching', "no");
         setSearchUsers([]);
     }
+
     async function unblock(e) {
         e.preventDefault();
         var result = await Axios.post('http://localhost:5000/unblock', {
@@ -74,6 +57,7 @@ function FriendsPage() {
         localStorage.setItem('blockedList', result.data.blockedList);
         setDoRender(e.target.value);
     }
+
     async function block(e) {
         e.preventDefault();
         var result = await Axios.post('http://localhost:5000/blockUser', {
@@ -85,6 +69,7 @@ function FriendsPage() {
         localStorage.setItem('blockedList', result.data.blockedList);
         setDoRender(e.target.value);
     }
+
     async function blockFromSearchBar(e) {
         e.preventDefault();
         var result = await Axios.post('http://localhost:5000/blockUser', {
@@ -99,6 +84,7 @@ function FriendsPage() {
         localStorage.setItem('isSearching', "no");
         setSearchUsers([]);
     }
+
     async function accept(e) {
         e.preventDefault();
         var result = await Axios.post('http://localhost:5000/acceptRequest', {
@@ -133,32 +119,7 @@ function FriendsPage() {
         }
     }
 
-    const FriendDisplay = (name) => {
-        return (
-        <HStack key={name} width="400px" spacing="10px" border-style="solid">
-            <Text width="200px">{name}</Text>
-            <Button value={name} align="right" color="blue" 
-                onClick={unfollow}>
-                    Unfollow</Button>
-            <Button value={name} align="right" color="blue"
-            onClick={block}>Block</Button>
-        </HStack>
-        );
-    }
-
-    function DisplayAllFriends() {
-        return (
-            <Box>
-            <l>
-                {friendsList.map( (name) => (
-                FriendDisplay(name)
-            ))}</l>
-            </Box>
-        );
-    }
-
     const BlockedDisplay = (name) => {
-        
         return (
         <HStack key={name} width="400px" spacing="10px" border-style="solid">
             <Text width="200px">{name}</Text>
@@ -168,118 +129,127 @@ function FriendsPage() {
     }
 
     function DisplayAllBlocked() {
-        return (
-            <Box>
-            <ul>
-            {
-                blockedList.map( (name) => (
-                BlockedDisplay(name)
-            ))}</ul>
-            </Box>
-        );
+        if(blockedList[0] !== ''){
+            return (
+                <Box>
+                <ul>
+                {
+                    blockedList.map( (name) => (
+                    BlockedDisplay(name)
+                ))}</ul>
+                </Box>
+            );
+        }else{
+            return (
+                <Text>You have not blocked any users</Text>
+            );
+       }
     }
 
     const RequestedDisplay = (name) => {
         return (
-        <Box borderStyle="solid">
-        <HStack key={name} width="400px" spacing="10px" >
-            <Text width="200px">{name}</Text>
-            <Button value={name} name="accept" align="right" color="blue" onClick={accept}>Accept</Button>
-            <Button value={name} name="deny" align="right" color="red" onClick={accept}>Deny</Button>
-        </HStack>
-        </Box>
+            <Box borderStyle="solid">
+            <HStack key={name} width="400px" spacing="10px" >
+                <Text width="200px">{name}</Text>
+                <Button value={name} name="accept" align="right" color="blue" onClick={accept}>Accept</Button>
+                <Button value={name} name="deny" align="right" color="red" onClick={accept}>Deny</Button>
+            </HStack>
+            </Box>
         )
     }
 
     function DisplayAllRequested() {
-        if (requestedBy.length === 0) {
-            return (<div></div>)
-        } else {
-        
-        return (
-            <Box id="displayRequested">
-                <ul>
-                    {
-                        requestedBy.map( (name) => (
-                        RequestedDisplay(name)
-                    ))}
-                </ul>
-            </Box>
-        );
-       }
-    }
-    const DisplaySearch = (name) => {
-        if ( name === username) {
-            return (<div></div>);
-        } else {
+        if (requestedBy[0] !== '') {
             return (
-                <Box border="1px" borderRadius="lg" m="5px 10px 5px 10px">
-                    <HStack key={name} padding="5px" spacing="10px">
-                        <FormLabel m="0 0 0 10px">{name}</FormLabel>
-                        <Button value={name} color="blue"  onClick={addToast}>Open Info</Button>
-                        <Button value={name} color="blue"  onClick={close}>Close Info</Button>
-                        <Button value={name} color="blue"  onClick={follow}>Follow</Button>
-                        <Button value={name} color="red" onClick={blockFromSearchBar}>Block</Button>
-                    </HStack>
-                </Box>
-            );
-        }
-    }
-
-    function DisplayAllSearch() {
-        if (localStorage.getItem('isSearching') === "no") {
-            return (<div></div>);
-        } else if (searchUsers.length === 0) {
-            return (<div></div>); 
-        }   else {
-            return (
-                <Box>
+                <Box id="displayRequested">
                     <ul>
                         {
-                            searchUsers.map( (name) => (
-                            DisplaySearch(name)
+                            requestedBy.map( (name) => (
+                            RequestedDisplay(name)
                         ))}
                     </ul>
                 </Box>
             );
-        }
+       }else{
+            return (
+                <Text>You currently have no friend requests</Text>
+            );
+       }
     }
+    
+    const DisplaySearch = (name) => {
+            return (
+                <Box border="1px" borderRadius="lg" m="5px 10px 5px 10px">
+                    <HStack key={name[0]} padding='5px'>
+                        <Image w='50px' h='50px' borderRadius='full' src={name[1]}/>
+                        <FormLabel>{name[2]}</FormLabel>
+                        <FormLabel>Ranking: {name[3]}</FormLabel>
+                        <Button value={name[2]} color="blue"  onClick={addToast}>Open Info</Button>
+                        <Button value={name[2]} color="blue"  onClick={close}>Close Info</Button>
+                        <Button value={name[2]} color="blue"  onClick={follow}>Follow</Button>
+                        <Button value={name[2]} color="red" onClick={blockFromSearchBar}>Block</Button>
+                    </HStack>
+                </Box>
+            );
+    }
+
+    function DisplayAllSearch() {
+        if(searchUsers.length !== 0){
+            return (
+                <Box>
+                    <VStack maxH="300px" overflow="hidden" overflowY="scroll" p={2}
+                        sx={{
+                            '&::-webkit-scrollbar': {
+                            width: '0px',
+                            backgroundColor: `transparent`,
+                            },
+                            '&::-webkit-scrollbar-thumb': {
+                            backgroundColor: `transparent`,
+                            },
+                        }}>
+                        {
+                            searchUsers.map( (name) => (
+                            DisplaySearch(name)
+                        ))}
+                    </VStack>
+                </Box>
+            );
+        }else{
+            return (
+                <Text>Use the searchbar to find other users</Text>
+            );
+       }
+        
+    }
+
     async function search(e) {
-       e.preventDefault();
-      
-        if (e.target.value !== "") {
+        e.preventDefault();
+        if (isNaN(e.target.value) && e.target.value !== '') {
             await Axios.post('http://localhost:5000/findTheUserReg', {
-				search: e.target.value,  
+                search: e.target.value,  
                 user: username
-			}).then(response => {
-                if (response.data.length !== 0) {
+            }).then(response => {
+                if(response.data.length !== 0) {
                     setSearchUsers(response.data);
-                }
-                else {
+                }else{
                     setSearchUsers([]);
                 }
             });
-
-       blockedList = localStorage.getItem('blockedList').split(",");
-       friendsList = localStorage.getItem('friendsList').split(",");
-       let blockedTemp = [];
-       let friendsTemp = [];
-        for (var i = 0; i < blockedList.length; i++) {                  
-            if (blockedList[i].includes(e.target.value) === true) {
-                blockedTemp.push(blockedList[i]);
-            }       
+        }else if(!isNaN(e.target.value) && e.target.value !== ''){
+            await Axios.post('http://localhost:5000/findTheUserReg', {
+                search: e.target.value,  
+                user: username
+            }).then(response => {
+                if(response.data.length !== 0) {
+                    setSearchUsers(response.data);
+                }else{
+                    setSearchUsers([]);
+                }
+            });
+        }else{
+            setSearchUsers([]);
+            localStorage.setItem('isSearching', "no");
         }
-        for (var j= 0; j < friendsList.length; j++) {
-            if (friendsList[j].includes(e.target.value) === true) {
-                friendsTemp.push(friendsList[j]);
-            }
-        }
-        localStorage.setItem('searchingBlocked', blockedTemp);
-        localStorage.setItem('searchingFriends', friendsTemp);
-        localStorage.setItem('isSearching', "yes");
-    } else {
-        localStorage.setItem('isSearching', "no");
-    } 
         setDoRender(e.target.value);
     }
 
@@ -288,102 +258,82 @@ function FriendsPage() {
     async function unsorted() {
         var result = await Axios.post('http://localhost:5000/getFriends', {
                 _id: id
-		    }); 
-            let friends = result.data;
-            for(var i = 0; i < friends.length; i++){
-                result = await Axios.post('http://localhost:5000/getFriendRanks', {
-                    _id: friends[i]
-                });
-                console.log(parseInt(result.data.ranking));
-                var rank = result.data.ranking + "";
-                let temp = [];
-                temp.push(result.data.user);
-                temp.push(rank);
-                friends[i] = temp;
-            }
-            localStorage.setItem('friends',friends);
-            window.location = "/friends";
+        }); 
+        let friends = result.data;
+        for(var i = 0; i < friends.length; i++){
+            result = await Axios.post('http://localhost:5000/getFriendRanks', {
+                _id: friends[i]
+            });
+            console.log(result.data);
+            let temp = [];
+            temp.push(result.data.user);
+            temp.push(result.data.ranking);
+            temp.push(result.data.image);
+            friends[i] = temp;
+        }
+        localStorage.setItem('friends',friends);
+        window.location = "/friends";
     }
 
     function ascending() {
         friends = localStorage.getItem('friends').split(",");
         let temp = [];
-        for(var i = 0; i < friends.length; i+=2){
-            temp.push([friends[i],friends[i+1]]);
+        for(var i = 0; i < friends.length; i+=3){
+            temp.push([friends[i],friends[i+1],friends[i+2]]);
         }
         for(var i = 0; i < temp.length; i++){
             for(var j = i; j < temp.length; j++){
                 if(temp[i][1] > temp[j][1]){
                     let k = temp[i][1];
                     let l = temp[i][0];
+                    let m = temp[i][2];
                     temp[i][1] = temp[j][1];
                     temp[i][0] = temp[j][0];
+                    temp[i][2] = temp[j][2];
                     temp[j][1] = k;
                     temp[j][0] = l;
+                    temp[j][2] = m;
                 }
             }
         }
-        let temp2 = [];
-        for(var i = 0; i < friends.length; i+=2){
-            temp2.push(
-                <Box>
-                    <Center>
-                        <FormLabel>Name: {friends[i]}</FormLabel>
-                        <FormLabel>Ranking: {friends[i+1]}</FormLabel>
-                    </Center>
-                </Box>
-            );
-        }
-        friends = temp2;
         localStorage.setItem('friends',temp);
-        console.log(temp);
         window.location = "/friends";
     }
 
     function descending() {
         friends = localStorage.getItem('friends').split(",");
         let temp = [];
-        for(var i = 0; i < friends.length; i+=2){
-            temp.push([friends[i],friends[i+1]]);
+        for(var i = 0; i < friends.length; i+=3){
+            temp.push([friends[i],friends[i+1],friends[i+2]]);
         }
         for(var i = 0; i < temp.length; i++){
             for(var j = i; j < temp.length; j++){
                 if(temp[i][1] < temp[j][1]){
                     let k = temp[i][1];
                     let l = temp[i][0];
+                    let m = temp[i][2];
                     temp[i][1] = temp[j][1];
                     temp[i][0] = temp[j][0];
+                    temp[i][2] = temp[j][2];
                     temp[j][1] = k;
                     temp[j][0] = l;
+                    temp[j][2] = m;
                 }
             }
         }
-        let temp2 = [];
-        for(var i = 0; i < friends.length; i+=2){
-            temp2.push(
-                <Box>
-                    <Center>
-                        <FormLabel>Name: {friends[i]}</FormLabel>
-                        <FormLabel>Ranking: {friends[i+1]}</FormLabel>
-                    </Center>
-                </Box>
-            );
-        }
-        friends = temp2;
         localStorage.setItem('friends',temp);
-        console.log(temp);
         window.location = "/friends";
     }
 
     if(friends[0] !== ''){
         let temp = [];
-        for(var i = 0; i < friends.length; i+=2){
+        for(var i = 0; i < friends.length; i+=3){
             temp.push(
-                <Box>
-                    <Center>
-                        <FormLabel>Name: {friends[i]}</FormLabel>
+                <Box border="1px" borderRadius="lg" m="5px 10px 5px 10px">
+                    <HStack padding='5px'>
+                        <Image w='50px' h='50px' borderRadius='full' src={friends[i+2]}/>                       <FormLabel>{friends[i]}</FormLabel>
                         <FormLabel>Ranking: {friends[i+1]}</FormLabel>
-                    </Center>
+                    </HStack>
                 </Box>
             );
         }
@@ -397,7 +347,7 @@ function FriendsPage() {
                 <Center padding="10px">
                     <Box w="80%" border="1px" borderRadius="lg">
                         <VStack m="10px 10px 10px 10px">
-                            <Input id="searchBar" name="searchBar" placeholder="Search for user" onChange={search}/>
+                            <Input id="searchBar" name="searchBar" placeholder="Search for user by 'name' or 'ranking #'" onChange={search}/>
                             <Divider/>
                             <DisplayAllSearch />
                         </VStack>
@@ -409,12 +359,12 @@ function FriendsPage() {
                             <VStack>
                             <FormLabel> My Friends</FormLabel>
                                 <HStack spacing="10px">
-                                    <Button id="ascending" onClick={unsorted}>Original</Button>
+                                    <Button id="unsorted" onClick={unsorted}>Original</Button>
                                     <Button id="ascending" onClick={ascending}>Ascending</Button>
                                     <Button id="descending" onClick={descending}>Descending</Button>
                                 </HStack>
                                 <Divider/>
-                                <VStack id="VStack">
+                                <VStack>
                                     {friends}
                                 </VStack>
                             </VStack>
